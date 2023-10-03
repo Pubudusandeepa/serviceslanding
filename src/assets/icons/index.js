4692c332f302fdb5b97ef57fddaf08acbd0d25ae
Pubudusandeepa <ppsandeepa12@gmail.com>
2023-10-04 01:25:57 +0530

    create heder responsive

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -4,5 +4,6 @@ import libry from "./library.png";
 import monitor from "./monitor.png";
 import paper from "./paper.png";
 import pig from "./pig.png";
+import list from "./list.png";
 
-export default { plane, diamond, libry, monitor, paper, pig };
+export default { plane, diamond, libry, monitor, paper, pig, list };
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import "./Header.css";
+import images from "../../assets/index.js";
 const Header = () => {
   const [colorChange, setColorchange] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
   const changeNavbarColor = () => {
     if (window.scrollY >= 80) {
       setColorchange(true);
@@ -10,6 +12,11 @@ const Header = () => {
     }
   };
   window.addEventListener("scroll", changeNavbarColor);
+
+  const toggleClicked = () => {
+    setIsClicked(!isClicked);
+  };
+
   return (
     <div className="header_section">
       <nav className="nav_bar">
@@ -33,6 +40,44 @@ const Header = () => {
           </div>
           <div className="buttons">
             <button>Try it Free</button>
+          </div>
+          <div
+            className="shadow"
+            style={{ backgroundColor: colorChange ? "black" : "" }}
+          >
+            {isClicked ? (
+              <div className="small_menu">
+                <div className="logo_small">
+                  <h2>Simplify</h2>
+                  <div className="small_logo" onClick={() => toggleClicked()}>
+                    <img src={images.icons.list} alt="navsmall" />
+                  </div>
+                </div>
+                <div className="nav_list_mini">
+                  <ul>
+                    <li>Home</li>
+                    <li>Services</li>
+                    <li>Features</li>
+                    <li>Pricing</li>
+                    <li>Team</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                  </ul>
+                </div>
+                <div className="buttons_mini">
+                  <button>Try it Free</button>
+                </div>
+              </div>
+            ) : (
+              <div className="small_nav">
+                <div className="logo_mini">
+                  <h2>Simplify</h2>
+                </div>
+                <div className="small_logo" onClick={() => toggleClicked()}>
+                  <img src={images.icons.list} alt="navsmall" />
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </nav>
